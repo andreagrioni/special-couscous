a1dbf312e879f990d016aef2ffb5638ee061b193
andrea grioni <grioni.andrea@gmail.com>
2020-02-21 23:50:27 +0100

    add anaconda installation

--- utilities/gcp_setup.sh
+++ utilities/gcp_setup.sh
@@ -1,7 +1,26 @@
-# set up script for Google Cloud Platform
+# set up Google Cloud Platform
+
+## presetting for static ip address here:
+# https://towardsdatascience.com/running-jupyter-notebook-in-google-cloud-platform-in-15-min-61e16da34d52
+
+## update ubuntu
+sudo apt-get update
+sudo apt-get upgrade
+
+## download Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+chmod +x Anaconda3-2019.10-Linux-x86_64.sh
+./Anaconda3-2019.10-Linux-x86_64.sh
+
+## set up jupyter notebook
+jupyter notebook --generate-config # creates confing file
+## set jupyter confing (port 8000)
+echo -e "c = get_config()\nc.NotebookApp.ip = '*'\nc.NotebookApp.open_browser = False\nc.NotebookApp.port = 8000" >> ~/.jupyter/jupyter_notebook_config.py
+## run jupyter
+#jupyter-notebook --no-browser --port=8000
+
 
 ## install GCSFUSE to mount Google buckets
-
 export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -9,15 +28,9 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install gcsfuse
 
-## install conda
-
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-
-bash Miniconda3-latest-Linux-x86_64.sh
-
 ## install git
 
 sudo apt install git
 
-git config --global user.email "grioni.andrea@gmail.com"
-git config --global user.name "andreagrioni"
+git config --global user.email "@gmail.com"
+git config --global user.name ""
